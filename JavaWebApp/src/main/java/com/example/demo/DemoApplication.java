a0b3c752059dbc5c10afda73e625ed694eef4b13
amymoh83 <amymoh83@gmail.com>
2022-11-07 05:17:43 +0000

    change

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		string name="moh"
+		string name = "moh"
 
 		system.out.println(name);
 	}
